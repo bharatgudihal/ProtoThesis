d5c85142545449cf00ea88b6106db5ff5955575d
Chuan-Chin Lai <u1070737@utah.edu>
2017-01-20 22:41:39 -0700

    updated gun function

---
+++ Assets/Scripts/bullet_destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_destroy : MonoBehaviour
+{
+	public float destroy_time = 2.0f;
+	// Use this for initialization
+	void Start ()
+    {
+		Destroy(gameObject, destroy_time);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
