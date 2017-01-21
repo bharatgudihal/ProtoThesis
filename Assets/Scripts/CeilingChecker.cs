a7bbcbfbe8bc5e8428f5e2a81172a9b533d9fffd
Bharat Gudihal <bharat.gudi@gmail.com>
2017-01-20 22:56:03 -0700

    ceiling removal protoscript done

---
+++ Assets/Scripts/CeilingChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.forward, out hit);
+        if(hit.transform.tag.Equals("Ceiling"))
+        {
+            hit.transform.gameObject.SetActive(false);
+        }
+	}
+}
