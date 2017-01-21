b8cd41c748317640e85c0ab91923a02d818cf6c2
Chuan-Chin Lai <u1070737@utah.edu>
2017-01-21 16:46:19 -0700

    update forcefield

---
+++ Assets/Scripts/Lai/ForceField.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour 
+{
+	public bool IsActive = true;
+
+	private KeyCode input = KeyCode.Z;
+	private GameObject m_Field;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Field = transform.GetChild (1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (input) && IsActive == true) 
+			m_Field.SetActive (true);
+		else 
+			m_Field.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(m_Field.activeSelf == true)
+			Destroy (other.gameObject);
+	}
+}
