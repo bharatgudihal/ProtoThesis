4fdde119799010931b0d0f05da93bbf6c27e5982
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2017-01-21 12:43:51 -0700

    circle bot

---
+++ Assets/Scripts/Bots/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+    [SerializeField] Rigidbody rigbod;
+    [SerializeField, Range(0f, 10f)] float xPeriod;
+    [SerializeField, Range(0f, 10f)] float zPeriod;
+    [SerializeField, Range(1f, 20f)] float xRadius;
+    [SerializeField, Range(1f, 20f)] float zRadius;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Mathf.Cos((2 * Mathf.PI / xPeriod) * Time.time) * xRadius;
+        float z = Mathf.Sin((2 * Mathf.PI / zPeriod) * Time.time) * zRadius;
+        rigbod.velocity = new Vector3(x,0f,z);
+
+    }
+}
