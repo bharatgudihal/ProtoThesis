c53cba7168414d287272cb2a23e2cba1b4598f61
Bharat Gudihal <bharat.gudi@gmail.com>
2017-01-21 11:05:49 -0700

    preliminary ceiling clearing script

--- Assets/Scripts/CeilingChecker.cs
+++ Assets/Scripts/CeilingChecker.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class CeilingChecker : MonoBehaviour {
 
+    GameObject currentCeiling;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        currentCeiling = null;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         RaycastHit hit;
-        Physics.Raycast(transform.position, transform.forward, out hit);
-        if(hit.transform.tag.Equals("Ceiling"))
+        Physics.Raycast(transform.position, transform.forward, out hit);            
+        if (hit.transform.tag.Equals("Ceiling"))
         {
-            hit.transform.gameObject.SetActive(false);
+            if (currentCeiling == null)
+            {
+                currentCeiling = hit.transform.gameObject;
+                Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
+                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+            }else if (currentCeiling.transform != hit.transform)
+            {
+                Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
+                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 1f);
+                currentCeiling = hit.transform.gameObject;
+                color = currentCeiling.GetComponent<MeshRenderer>().material.color;
+                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+            }
         }
 	}
 }
