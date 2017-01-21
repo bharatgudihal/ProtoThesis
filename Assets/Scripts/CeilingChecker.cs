80c7469cf7038448bac9279f7ce2f1cd166bc048
Bharat Gudihal <bharat.gudi@gmail.com>
2017-01-21 15:40:14 -0700

    xray stuff

--- Assets/Scripts/CeilingChecker.cs
+++ Assets/Scripts/CeilingChecker.cs
@@ -4,32 +4,57 @@ using UnityEngine;
 
 public class CeilingChecker : MonoBehaviour {
 
+    GameObject previousCeiling;
     GameObject currentCeiling;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    float fadeInSpeed;
+
+    // Use this for initialization
+    void Start () {
+        previousCeiling = null;
         currentCeiling = null;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        CheckCeilingThroughRayCast();
+        StartFadingIn();
+	}
+
+    void CheckCeilingThroughRayCast()
+    {
         RaycastHit hit;
-        Physics.Raycast(transform.position, transform.forward, out hit);            
-        if (hit.transform.tag.Equals("Ceiling"))
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
-            if (currentCeiling == null)
+            if (hit.transform.tag.Equals("Ceiling"))
             {
-                currentCeiling = hit.transform.gameObject;
-                Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
-                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
-            }else if (currentCeiling.transform != hit.transform)
-            {
-                Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
-                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 1f);
-                currentCeiling = hit.transform.gameObject;
-                color = currentCeiling.GetComponent<MeshRenderer>().material.color;
-                currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+                if (currentCeiling == null)
+                {
+                    currentCeiling = hit.transform.gameObject;
+                    Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
+                    currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+                }
+                else if (currentCeiling.transform != hit.transform)
+                {
+                    previousCeiling = currentCeiling;
+                    currentCeiling = hit.transform.gameObject;
+                    Color color = currentCeiling.GetComponent<MeshRenderer>().material.color;
+                    currentCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+                }
             }
         }
-	}
+    }
+
+    void StartFadingIn()
+    {
+        if(previousCeiling != null)
+        {
+            Color color = previousCeiling.GetComponent<MeshRenderer>().material.color;
+            if (color.a != 1f)
+            {
+                previousCeiling.GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, Mathf.Lerp(color.a, 1f, fadeInSpeed));
+            }
+        }
+    }
 }
