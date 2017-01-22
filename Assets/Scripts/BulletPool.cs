6a108a3531f4e927cfa48832c94adfd1a9a70ae3
Bharat Gudihal <bharat.gudi@gmail.com>
2017-01-22 12:08:58 -0700

    bulletpool done

--- Assets/Scripts/BulletPool.cs
+++ Assets/Scripts/BulletPool.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletPool : MonoBehaviour {
+public class BulletPool : MonoBehaviour
+{
+    public static BulletPool instance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    GameObject prefab;
+
+    [SerializeField]
+    int poolSize;
+
+    List<GameObject> bulletPool;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(instance);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject bullet = Instantiate(prefab);
+            bullet.SetActive(false);
+            bulletPool.Add(bullet);
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        GameObject bullet = null;
+        for(int i = 0; i < poolSize; i++)
+        {
+            if (!bulletPool[i].activeInHierarchy)
+            {
+                bullet = bulletPool[i];
+                break;
+            }
+        }
+        return bullet;
+    }    
+
 }
+
