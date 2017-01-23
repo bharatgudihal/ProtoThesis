1d5432958cb0483085e77c6da889b27e2a3cf6b8
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2017-01-22 17:28:24 -0700

    jetpack-type mod set up

---
+++ Assets/Scripts/Mods/Jetpack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack : Mod
+{
+
+    [SerializeField, Range(10f, 1000f)]
+    float thrustForce;
+    bool canActivate = true;
+
+
+    public override void Activate()
+    {
+        if (canActivate)
+        {
+            canActivate = false;
+            Vector3 forceDirection = Camera.main.transform.TransformDirection(-transform.parent.forward * thrustForce);
+            joystickMovement.AddJetForce(forceDirection, myModSpot);            
+        }
+    }
+
+    void ReActivate()
+    {
+        canActivate = true;
+    }
+
+    public override void DeActivate()
+    {
+        canActivate = true;
+        joystickMovement.StopConstantForce(myModSpot);
+    }
+
+    public override void Fatigue()
+    {
+
+    }
+}
+
